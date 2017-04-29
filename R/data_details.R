47f3e44b19b46189298a8ba9d4871b57ad9170fb
cimentadaj <cimentadaj@gmail.com>
2017-04-29 20:39:29 +0200

    Start of repo

---
+++ R/data_details.R
@@ -0,0 +1,24 @@
+#' Economic inequality indicators for all available PISA waves (2000 to 2015 every three years)
+#'
+#' This datasets contains 9 economic inequality indicators for over 30 countries
+#' for all available PISA waves. It is taken from the OECD website. For years
+#' where no data was available, the closest year was taken. For more detail please
+#' check out the 'generating_database.R' script from the Github repository.
+#'
+#'
+#' @format A tibble with 1,776 rows and 4 columns:
+#' \describe{
+#'   \item{GINI}{Gini (disposable income, post taxes and transfers)}
+#'   \item{GINIB}{Gini (market income, before taxes and transfers)}
+#'   \item{GINIG}{Gini (gross income, before taxes)}
+#'   \item{PALMA}{Palma ratio)}
+#'   \item{P90P10}{P90/P10 disposable income decile ratio}
+#'   \item{P90P50}{P90/P50 disposable income decile ratio}
+#'   \item{P50P10}{P50/P10 disposable income decile ratio}
+#'   \item{S80S20}{S80/S20 disposable income quintile share}
+#'   \item{S90S10}{S90S10	S90/S10 disposable income decile share}
+#' }
+#' @source \url{https://stats.oecd.org/Index.aspx?DataSetCode=IDD}
+#'
+#'
+"inequality_data"
